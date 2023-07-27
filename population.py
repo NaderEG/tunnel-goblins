a0755cb69e4051a96d28f7a773c573051c3aae80
Nader El-Ghotmi <43392069+NaderEG@users.noreply.github.com>
2023-07-26 23:01:26 -0400

    goblin class

---
+++ population.py
@@ -0,0 +1,34 @@
+import random
+
+class Goblin:
+	def __init__(self, dnaSize, dna=[]):
+		if dna:
+			self.dna = dna
+		else:
+			options = ["right", "left", "up", "down"]
+			self.dna = random.choices(options, k=dnaSize)
+		self.fitness = 0
+		self.x = self.y = 0
+
+	def reproduce(self, other):
+		splitPoint = random.randint(0, len(self.dna)-1)
+		newDna = self.dna[0:splitPoint] + other.dna[splitPoint:]
+		return Goblin(len(newDna), newDna)
+
+	def step(self, m):
+
+
+	def canMove(self, m, d):
+		if d == "up" and self.y - 1 >= 0:
+			return not m.grid[self.x + self.y*m.cols.walls["top"]]
+		elif d == "down" and self.y + 1 < rows:
+			return not m.grid[self.x + self.y*m.cols.walls["bottom"]]
+		elif d == "left" and self.y - 1 >=0:
+			return not m.grid[self.x + self.y*m.cols.walls["left"]]
+		elif d == "right" and self.y + 1 < cols:
+			return not m.grid[self.x + self.y*m.cols.walls["right"]]
+
+	def getDistance(self, m):
+		return ((m.cols-1 - self.x)**2 + (m.rows-1 - self.y)**2)**0.5 
+
+
